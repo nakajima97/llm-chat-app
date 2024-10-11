bd1cb0ec3b8ae71212e4b4787dc3d2d1f5b0be4a
Nakajima <kituneudon97@gmail.com>
2024-10-11 09:09:03 +0900

    MUIを導入 (#2)
    * #1 MUIを導入した
    
    * #1 biomeの実行

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,18 @@
+import {
+	DocumentHeadTags,
+	documentGetInitialProps,
+} from '@mui/material-nextjs/v14-pagesRouter';
+import type { DocumentHeadTagsProps } from '@mui/material-nextjs/v14-pagesRouter';
 import { Head, Html, Main, NextScript } from 'next/document';
+import type { DocumentContext } from 'next/document';
+import type { DocumentProps } from 'next/document';
 
-export default function Document() {
+export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
 	return (
-		<Html lang="en">
-			<Head />
+		<Html lang="ja">
+			<Head>
+				<DocumentHeadTags {...props} emotionStyleTags={[]} />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
@@ -11,3 +20,8 @@ export default function Document() {
 		</Html>
 	);
 }
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+	const finalProps = await documentGetInitialProps(ctx);
+	return finalProps;
+};

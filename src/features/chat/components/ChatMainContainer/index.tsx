d2608a55906aa7c7243cd44cfaba0cb730b5c26d
Nakajima <kituneudon97@gmail.com>
2024-11-16 16:46:59 +0900

    スレッドIDを送信するようにした (#34)
    * #23 スレッドIDを送ることを確認
    
    * #23 biomeの設定更新
    
    * #23 名称の重複を排除

--- src/features/chat/components/ChatMainContainer/index.tsx
+++ src/features/chat/components/ChatMainContainer/index.tsx
@@ -10,28 +10,50 @@ export const ChatMainContainer = () => {
 		sendChat,
 		latestQuestion,
 		latestAnswer,
-		threadId,
+		currentThreadId,
 		clearLatestAnswer,
 	} = useSendChat();
 	const { chatHistory, appendChats } = useChatHistory();
 
 	const router = useRouter();
 
+	/**
+	 * 現在のthreadIdでURLパラメータの値を更新する。
+	 */
 	useEffect(() => {
-		if (threadId && router.query.thread !== threadId) {
+		if (currentThreadId && router.query.thread !== currentThreadId) {
 			router.replace(
 				{
 					pathname: router.pathname,
-					query: { ...router.query, thread: threadId },
+					query: { ...router.query, thread: currentThreadId },
 				},
 				undefined,
 				{ shallow: true },
 			);
 		}
-	}, [threadId, router.pathname, router.replace, router.query]);
+	}, [currentThreadId, router.pathname, router.replace, router.query]);
 
+	/**
+	 * ルータークエリからthreadIdを取得します。
+	 * @returns {string | undefined} 利用可能な場合、threadIdを返します。
+	 */
+	const getThreadId = (): string | undefined => {
+		const threadId = router.query.thread;
+
+		if (Array.isArray(threadId)) {
+			return threadId[0];
+		}
+		return threadId;
+	};
+
+	/**
+	 * チャットメッセージを送信します。
+	 * @param {SendChatArgument} param0 - 送信するメッセージ。
+	 */
 	const handleSendChat = ({ message }: SendChatArgument) => {
-		sendChat({ message });
+		const threadId = getThreadId();
+
+		sendChat({ message, threadId });
 		clearLatestAnswer();
 	};
 

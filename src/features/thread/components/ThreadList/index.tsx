312634d0549af0b2f56cff3d5ddc8ba0c52ba10e
nakajima97 <kituneudon97@gmail.com>
2024-11-28 09:00:17 +0900

    #45 スクロールの実装

--- src/features/thread/components/ThreadList/index.tsx
+++ src/features/thread/components/ThreadList/index.tsx
@@ -1,4 +1,10 @@
-import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from '@mui/material';
 import type { ThreadListType } from '../../types';
 
 type Props = {
@@ -7,14 +13,22 @@ type Props = {
 
 export const ThreadList = ({ threads }: Props) => {
   return (
-    <List sx={{ width: '100%', maxWidth: 360 }}>
-      {threads.map((thread) => (
-        <ListItem key={thread.id}>
-          <ListItemButton>
-            <ListItemText primary={thread.title} />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
+    <Box
+      sx={{
+        width: '100%',
+        height: '100%',
+        overflowY: 'auto',
+      }}
+    >
+      <List sx={{ width: '100%', maxWidth: 360 }}>
+        {threads.map((thread) => (
+          <ListItem key={thread.id}>
+            <ListItemButton>
+              <ListItemText primary={thread.title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   );
 };

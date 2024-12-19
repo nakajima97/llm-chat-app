682c83b83e9afe320c11cf5736ff368261266cc9
Nakajima <kituneudon97@gmail.com>
2024-12-19 18:22:06 +0900

    チャット送信時に画面がちらつくバグに対応 (#67)
    * #61 新規チャットでチャットを送った際に画面がチカチカするバグに対応
    
    * #61 不要な処理を削除
    
    * 不要なインポート文の削除

--- src/features/chat/hooks/useThreadId.ts
+++ src/features/chat/hooks/useThreadId.ts
@@ -1,47 +1,57 @@
+import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/router';
 import { useCallback, useEffect, useState } from 'react';
 
 export const useThreadId = () => {
   const [threadId, setThreadId] = useState<string | undefined>(undefined);
   const router = useRouter();
+  const searchParams = useSearchParams();
+
+  /**
+   * ルータークエリからthreadIdを取得します。
+   * @returns {string} 利用可能な場合、threadIdを返します。
+   */
+  const getThreadId = useCallback((): string => {
+    const queryThreadId = searchParams.get('thread-id');
+
+    if (Array.isArray(queryThreadId)) {
+      return queryThreadId[0];
+    }
+    return queryThreadId ?? '';
+  }, [searchParams.get]);
 
   /**
    * ページを開いた際にURLパラメータからthreadIdを取得します。
    */
   useEffect(() => {
     if (router.isReady) {
-      const id = getThreadId();
-      setThreadId(id);
+      const queryThreadId = searchParams.get('thread-id');
+      if (queryThreadId) {
+        setThreadId(queryThreadId);
+      }
     }
-  }, [router.isReady]);
-
-  /**
-   * ルータークエリからthreadIdを取得します。
-   * @returns {string | undefined} 利用可能な場合、threadIdを返します。
-   */
-  const getThreadId = useCallback((): string | undefined => {
-    const queryThreadId = router.query.threadId;
-
-    if (Array.isArray(queryThreadId)) {
-      return queryThreadId[0];
-    }
-    return queryThreadId;
-  }, [router.query.threadId]);
+  }, [router.isReady, searchParams]);
 
   /**
    * 現在のthreadIdでURLパラメータの値を更新する。
    */
-  useEffect(() => {
-    if (threadId) {
-      router.replace(
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  const replace = useCallback(router.replace.bind(router), [router.replace]);
+
+  const updateUrl = useCallback(
+    (id: string) => {
+      replace(
         {
-          pathname: `/chat/${threadId}`,
+          pathname: '/chat',
+          query: { 'thread-id': id },
         },
         undefined,
         { shallow: true },
       );
-    }
-  }, [threadId, router.replace]);
+    },
+    [replace],
+  );
 
   return { threadId, setThreadId, getThreadId };
 };

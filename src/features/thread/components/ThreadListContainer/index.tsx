134f55cb22c62b3495534a8bd0d58c6840051fc9
nakajima97 <kituneudon97@gmail.com>
2025-01-11 17:10:31 +0900

    #82 確認ダイアログを追加

--- src/features/thread/components/ThreadListContainer/index.tsx
+++ src/features/thread/components/ThreadListContainer/index.tsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import { useDeleteAllThread } from '../../hooks/useDeleteAllThread';
 import { useDeleteThread } from '../../hooks/useDeleteThread';
 import { useFetchThreadList } from '../../hooks/useFetchThreadList';
+import { DeleteAllThreadDialog } from '../DeleteAllThreadDialog';
 import { ThreadList } from '../ThreadList';
 
 export const ThreadListContainer = () => {
   const { fetchThreadList } = useFetchThreadList();
   const { mutateAsync } = useDeleteThread();
   const { mutateAsync: deleteAllThread } = useDeleteAllThread();
+  const [open, setOpen] = useState(false);
 
   const { data, refetch } = fetchThreadList();
 
@@ -15,16 +18,28 @@ export const ThreadListContainer = () => {
     refetch();
   };
 
+  const openDeleteAllThreadDialog = () => {
+    setOpen(true);
+  };
+
   const handleAllDelete = async () => {
     await deleteAllThread();
     refetch();
+    setOpen(false);
   };
 
   return (
-    <ThreadList
-      threads={data ?? []}
-      handleDelete={handleDelete}
-      handleAllDelete={handleAllDelete}
-    />
+    <>
+      <ThreadList
+        threads={data ?? []}
+        handleDelete={handleDelete}
+        handleAllDelete={openDeleteAllThreadDialog}
+      />
+      <DeleteAllThreadDialog
+        open={open}
+        onClose={() => setOpen(false)}
+        onConfirm={handleAllDelete}
+      />
+    </>
   );
 };
